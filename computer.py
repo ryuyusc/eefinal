6c4b76e07d55126d1af1214b722addec45ee1367
Youngbin Ryu <ryuyb1105@gmail.com>
2023-12-01 16:28:50 -0800

    initial test

---
+++ computer.py
@@ -0,0 +1,44 @@
+import paho.mqtt.client as mqtt
+from cryptography.fernet import Fernet
+from datetime import datetime
+import time
+
+#encryption key
+key = b'abcdefghi'
+cipher = Fernet(key)
+
+# creates broker address and topic
+broker = "test.mosquitto.org"
+topic = "final"
+
+#callback for when connected
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server with result code " + str(rc))
+    client.subscribe(topic)
+
+
+#callback for when message received
+def on_message(client, userdata, msg):
+    #decrypts data
+    decrypted = cipher.decrypt(msg.payload)
+
+    #converts bytes to tuple
+    message = eval(decrypted.decode())
+
+    print(message)
+    print(datetime.now())
+    
+
+if __name__ == '__main__':
+    # creates client and callback
+    client = mqtt.Client()
+    client.on_message = on_message
+
+    #connect to broker
+    client.connect(broker, 1883, 60)
+
+    client.loop_start()
+
+    while True:
+        time.sleep(1)
+
---
+++ rpi.py
@@ -0,0 +1,102 @@
+import paho.mqtt.client as mqtt
+from cryptography.fernet import Fernet
+import grovepi
+import geocoder
+import requests
+import time
+
+# grovepi button port
+BT_PORT = 3
+
+# grovepi temperature and humidity port
+TEMP_PORT = 2
+
+# this is for encryption purposes
+key = b'abcdefghi'
+cipher = Fernet(key)
+
+# creates broker address and topic
+broker = "test.mosquitto.org"
+topic = "final"
+
+# API website and key
+API_SITE = "https://openweather.org/current"
+API_KEY = '4cc919c4981eb59f9f4ffd541c6a1626'
+
+
+# connection callback
+def on_connect(client, userdata, flags, rc):
+    print("Connected to server with result code" + str(rc))
+
+# callback when message published
+def on_publish(client, userdata, mid):
+    print("sent")
+
+# grabbing data from the OpenWeather API
+#returns tuple of [temperature, humidity]
+def get_data():
+    # grabs location information using geocoder
+    location = geocoder.ip('me')
+    lat = location.latlng[0]
+    long = location.latlng[1]
+
+    #creates parameters
+    params = {
+        'appid': API_KEY,
+        'lat': lat,
+        'lon': long,
+        'units': 'imperial',
+    }
+
+    response = requests.get(API_SITE)
+
+    #extracts temperature and humidity if successful
+    if response.status_code == 200:
+        data = response.json()
+        return (data.get("main").get("temp"), data.get("main").get("humidity"))
+    
+    #error
+    else:
+        print('error: got response code %d' % response.status_code)
+        print(response.text)
+        return 0.0, 0.0
+
+
+
+
+
+if __name__ == '__main__':
+
+    #sets the pins
+    grovepi.pinMode(BT_PORT, "INPUT")
+    grovepi.pinMode(TEMP_PORT, "INPUT")
+
+    # creates MQTT client
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+    client.connect(host=broker, port=1883, keepalive = 60)
+    client.loop_start()
+
+    client.on_publish = on_publish
+
+    # starts loop of checking whether button is pressed
+    while True:
+        if (grovepi.digitalRead(BT_PORT) == 1):
+            #grabs data from the temperature and humidity sensor
+            [temperature, humidity] = grovepi.dht(TEMP_PORT, 0)
+
+            # grabs data from the openweather API
+            api_temp, api_hum = get_data()
+
+            # creates tuple to send that has all the data
+            data_toSend = (temperature , humidity, api_temp, api_hum)
+
+            #convert data to bytes
+            data_bytes = str(data_toSend).encode()
+
+            #encryption
+            encrypted = cipher.encrypt(data_bytes)
+
+            client.publish(topic, encrypted)
+            time.sleep(1)
